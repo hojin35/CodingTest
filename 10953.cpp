ec7d6b3dbf56642a96df282e1f9588c4d76e0fa5
hojin35 <66587008+hojin35@users.noreply.github.com>
2023-01-11 15:11:21 +0900

    2023 1/11

---
+++ 10953.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num;
+	cin >> num;
+	while (num--)
+	{
+		int n;
+		cin >> n;
+		char x;
+		cin >> x;
+		int n1;
+		cin >> n1;
+
+		cout << n + n1 << '\n';
+	}
+
+	
+}
